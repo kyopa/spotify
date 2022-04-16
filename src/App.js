f98aedfc040a9dd4b5c0c9564d594916bc383e76
kyopa <pomelleero@gmail.com>
2022-04-16 22:59:24 +0300

    fetchin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+
+
+  const [token, setToken] = useState()
+
+
+
+  const client_id = "de8735e4e92a4bbe8bb99948f6dfbb1d"
+  const client_secret = "7a1e27603bd84785a7d5b3465a5cbfe1"
+
+  
+
+  useEffect(() => {
+    fetch('https://accounts.spotify.com/api/token', {
+      method: "POST",
+      body: "grant_type=client_credentials",
+      headers: {
+        "Authorization": `Basic ` + window.btoa(`${client_id}:${client_secret}`),
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+    })
+    .then(data => data.json())
+    .then(res => {
+      fetch("https://api.spotify.com/v1/search?q=travis scott&type=track%2Cartist%2Calbum%2Cplaylist%2Cshow%2Cepisode", {
+        headers: {
+          "Accept": "application/json",
+          Authorization: `Bearer ${res.access_token}`,
+          "Content-Type": "application/json"
+        }
+      })
+      .then(data => data.json())
+      .then(res => console.log(res))
+    })
+
+
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello wrod
     </div>
-  );
+  )
 }
+
+
 
 export default App;
